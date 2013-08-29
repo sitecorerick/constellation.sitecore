edd1786f2625ae265283051eeced7d122cf1aaa5
Richard Cabral <richard@viridianspark.com>
2013-08-29 13:01:19 -0400

    Adding the basic IContextSensitive classes.

---
+++ Rules/Actions/RuleAction.cs
@@ -0,0 +1,203 @@
+﻿namespace Spark.Sitecore.Rules.Actions
+{
+	using System.Collections.Generic;
+	using System.Web;
+
+	using global::Sitecore.Diagnostics;
+
+	/// <summary>
+	/// A basic RuleAction framework that prevents rule recursion.
+	/// </summary>
+	/// <typeparam name="T">The ruleContext.</typeparam>
+	public abstract class RuleAction<T> : global::Sitecore.Rules.Actions.RuleAction<T>, IContextSensitive
+		where T : global::Sitecore.Rules.RuleContext
+	{
+		#region Locals
+		/// <summary>
+		/// Lists IDs that are still being acted upon. Prevents recursion.
+		/// </summary>
+		// ReSharper disable StaticFieldInGenericType
+		private static readonly List<string> InProgress = new List<string>();
+		// ReSharper restore StaticFieldInGenericType
+		#endregion
+
+		#region Properties
+		/// <summary>
+		/// Gets the Context Validator associated with the instance.
+		/// </summary>
+		public ContextValidator ContextValidator { get; private set; }
+
+		/// <summary>
+		/// Gets the Sitecore database name for the current request.
+		/// </summary>
+		public string ContextDatabaseName
+		{
+			get { return global::Sitecore.Context.Database != null ? global::Sitecore.Context.Database.Name : string.Empty; }
+		}
+
+		/// <summary>
+		/// Gets the Host name of the current request.
+		/// </summary>
+		public string ContextHostName
+		{
+			get { return HttpContext.Current != null ? HttpContext.Current.Request.Url.Host : string.Empty; }
+		}
+
+		/// <summary>
+		/// Gets the file path for the current request.
+		/// </summary>
+		public string ContextLocalPath
+		{
+			get { return HttpContext.Current != null ? HttpContext.Current.Request.Url.LocalPath : string.Empty; }
+		}
+
+		/// <summary>
+		/// Gets the Sitecore site name for the current request.
+		/// </summary>
+		public string ContextSiteName
+		{
+			get { return global::Sitecore.Context.Site != null ? global::Sitecore.Context.Site.Name : string.Empty; }
+		}
+
+		/// <summary>
+		/// <para>
+		/// Gets or sets a comma-delimited list of Database names that should match the current Request before
+		/// the Processor executes.
+		/// </para>
+		/// <para>
+		/// When implementing, list the members in alphabetical order to optimize search routines.
+		/// </para>
+		/// </summary>
+		public string DatabasesToProcess { get; set; }
+
+		/// <summary>
+		/// <para>
+		/// Gets or sets a comma-delimited list of Database names that should cause the Processor to ignore
+		/// the request.
+		/// </para>
+		/// <para>
+		/// When implementing, list the members in alphabetical order to optimize search routines.
+		/// </para>
+		/// </summary>
+		public string DatabasesToIgnore { get; set; }
+
+		/// <summary>
+		/// <para>
+		/// Gets or sets a comma-delimited list of host names that should match the current Request before
+		/// the Processor executes.
+		/// </para>
+		/// <para>
+		/// When implementing, list the members in alphabetical order to optimize search routines.
+		/// </para>
+		/// </summary>
+		public string HostnamesToProcess { get; set; }
+
+		/// <summary>
+		/// <para>
+		/// Gets or sets a comma-delimited list of host names that should cause the Processor to ignore
+		/// the request.
+		/// </para>
+		/// <para>
+		/// When implementing, list the members in alphabetical order to optimize search routines.
+		/// </para>
+		/// </summary>
+		public string HostnamesToIgnore { get; set; }
+
+		/// <summary>
+		/// <para>
+		/// Gets or sets a comma-delimited list of file paths that should match the current Request before
+		/// the Processor executes.
+		/// </para>
+		/// <para>
+		/// When implementing, list the members in alphabetical order to optimize search routines.
+		/// </para>
+		/// </summary>
+		public string PathsToProcess { get; set; }
+
+		/// <summary>
+		/// <para>
+		/// Gets or sets a comma-delimited list of file paths that should cause the Processor to ignore 
+		/// the request.
+		/// </para>
+		/// <para>
+		/// When implementing, list the members in alphabetical order to optimize search routines.
+		/// </para>
+		/// </summary>
+		public string PathsToIgnore { get; set; }
+
+		/// <summary>
+		/// <para>
+		/// Gets or sets a comma-delimited list of Site names that should match the current Request before
+		/// the Processor executes.
+		/// </para>
+		/// <para>
+		/// When implementing, list the members in alphabetical order to optimize search routines.
+		/// </para>
+		/// </summary>
+		public string SitesToProcess { get; set; }
+
+		/// <summary>
+		/// <para>
+		/// Gets or sets a comma-delimited list of Site names that should cause the Processor to ignore
+		/// the request.
+		/// </para>
+		/// <para>
+		/// When implementing, list the members in alphabetical order to optimize search routines.
+		/// </para>
+		/// </summary>
+		public string SitesToIgnore { get; set; }
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="RuleAction{T}"/> class.
+		/// </summary>
+		// ReSharper disable PublicConstructorInAbstractClass
+		public RuleAction()
+		// ReSharper restore PublicConstructorInAbstractClass
+		{
+			this.ContextValidator = new ContextValidator(this);
+		}
+		#endregion
+
+		#region Methods
+		/// <summary>
+		/// The method called by the Rule Engine.
+		/// </summary>
+		/// <param name="ruleContext">The context.</param>
+		public override void Apply(T ruleContext)
+		{
+			if (!this.ContextValidator.ContextIsValidForExecution())
+			{
+				return;
+			}
+
+			if (InProgress.Contains(ruleContext.Item.ID.ToString()))
+			{
+				return;
+			}
+
+			Log.Debug("RuleAction " + this.GetType().Name + " started for " + ruleContext.Item.Name, this);
+			InProgress.Add(ruleContext.Item.ID.ToString());
+
+			try
+			{
+				this.Execute(ruleContext);
+			}
+			finally
+			{
+				InProgress.Remove(ruleContext.Item.ID.ToString());
+			}
+
+			Log.Debug("RuleAction " + this.GetType().Name + " ended for " + ruleContext.Item.Name, this);
+		}
+
+		/// <summary>
+		/// Implement the Action details.
+		/// </summary>
+		/// <param name="ruleContext">The context.</param>
+		protected abstract void Execute(T ruleContext);
+		#endregion
+	}
+}
---
+++ Rules/Conditions/ItemDerivesFromTemplate.cs
@@ -0,0 +1,70 @@
+﻿namespace Spark.Sitecore.Rules.Conditions
+{
+	using global::Sitecore.Data;
+	using global::Sitecore.Diagnostics;
+	using global::Sitecore.Rules;
+	using global::Sitecore.Rules.Conditions;
+
+	/// <summary>
+	/// Rule that walks the supplied Item's template inheritance hierarchy looking
+	/// for a match to the TemplateID provided by the rule context.
+	/// </summary>
+	/// <typeparam name="T">Instance of Sitecore.Rules.Conditions.RuleContext.</typeparam>
+	public class ItemDerivesFromTemplate<T> : WhenCondition<T>
+		where T : RuleContext
+	{
+		/// <summary>
+		/// The template Id.
+		/// </summary>
+		private ID templateId;
+
+		/// <summary>
+		/// Gets or sets the template id.
+		/// </summary>
+		public ID TemplateId
+		{
+			get
+			{
+				return this.templateId;
+			}
+
+			set
+			{
+				Assert.ArgumentNotNull(value, "value");
+				this.templateId = value;
+			}
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the ItemDerivesFromTemplate class.
+		/// </summary>
+		public ItemDerivesFromTemplate()
+		{
+			this.templateId = ID.Null;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the ItemDerivesFromTemplate class.
+		/// </summary>
+		/// <param name="templateId">The template id.</param>
+		public ItemDerivesFromTemplate(ID templateId)
+		{
+			Assert.ArgumentNotNull(templateId, "templateId");
+			this.templateId = templateId;
+		}
+
+		/// <summary>
+		/// Executes the specified rule context.
+		/// </summary>
+		/// <param name="ruleContext">The rule context.</param>
+		/// <returns>
+		/// <c>True</c>, if the condition succeeds, otherwise <c>false</c>.
+		/// </returns>
+		protected override bool Execute(T ruleContext)
+		{
+			Assert.ArgumentNotNull(ruleContext, "ruleContext");
+			var item = ruleContext.Item;
+			return item != null && item.IsDerived(this.TemplateId);
+		}
+	}
+}
