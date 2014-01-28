6109ec18ab640cc16b04f250eb4cbfda5b2f4ad0
Richard Cabral <sitecorerick@gmail.com>
2014-01-28 12:42:43 -0500

    Nuget support.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Spark.Sitecore")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Basic library extending the ASP.NET Sitecore CMS. A dependency of other Spark.Sitecore libraries.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Richard J. Cabral")]
 [assembly: AssemblyProduct("Spark.Sitecore")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
